d7cabea4334026931199611b144137a5dbd99c79
rmsdnjs518 <rmsdnjs518@gmail.com>
2022-08-19 13:36:17 +0900

    bottom sheet update

--- ins_note/lib/main.dart
+++ ins_note/lib/main.dart
@@ -1,115 +1,172 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_bar_with_sheet/bottom_bar_with_sheet.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const BaseExample());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class BaseExample extends StatelessWidget {
+  const BaseExample({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        iconTheme: const IconThemeData(
+          color: Colors.blue,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
+
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>{
+  final _bottomBarController = BottomBarWithSheetController(initialIndex: 0);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState(){
+    _bottomBarController.stream.listen((opened) {
+      debugPrint('Bottom bar ${opened ? 'opened' : 'closed'}');
     });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[700],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () => _bottomBarController.openSheet(),
+              child: const Text('Open sheet'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              onPressed: () => _bottomBarController.closeSheet(),
+              child: const Text('Close sheet'),
+            ),
+            ElevatedButton(
+              onPressed: () => _bottomBarController.toggleSheet(),
+              child: const Text('Toggle sheet'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "Place for your content",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomBarWithSheet(
+        controller: _bottomBarController,
+        bottomBarTheme: const BottomBarTheme(
+          mainButtonPosition: MainButtonPosition.middle,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
+          ),
+          itemIconColor: Colors.grey,
+          itemTextStyle: TextStyle(
+            color: Colors.grey,
+            fontSize: 10.0,
+          ),
+          selectedItemTextStyle: TextStyle(
+            color: Colors.blue,
+            fontSize: 10.0,
+          ),
+        ),
+        onSelectItem: (index) => debugPrint('$index'),
+        sheetChild: Column(
+          children: <Widget>[
+            Padding(
+              child:
+              TextFormField(
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.lightbulb),
+                    hintText: 'Key Word',
+                    labelText: 'Key Word',
+
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Colors.black),
+                    )
+                ),
+                onSaved: (value){
+                  print("Key world onSaved:$value");
+                },
+                validator: (value){
+                  if(value!.isEmpty){
+                    return "Please enter some Key word";
+                  }
+                  return null;
+                },
+                onFieldSubmitted: (value){
+                  print('submitted:$value');
+                },
+              ),
+              padding: EdgeInsets.zero,
+            ),
+            Padding(
+              child:
+              TextFormField(
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.notes_outlined),
+                    hintText: 'Detail Note',
+                    labelText: 'Detail Note',
+
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Colors.black),
+                    )
+                ),
+                onSaved: (value){
+                  print("Detail Note onSaved:$value");
+                },
+                validator: (value){
+                  if(value!.isEmpty){
+                    return "Please enter some Detail note";
+                  }
+                  return null;
+                },
+                onFieldSubmitted: (value){
+                  print('submitted:$value');
+                },
+              ),
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            ),
+          ],
+        ),
+        items: const [
+          BottomBarWithSheetItem(icon: Icons.people),
+          //BottomBarWithSheetItem(icon: Icons.shopping_cart),
+          //BottomBarWithSheetItem(icon: Icons.settings),
+          BottomBarWithSheetItem(icon: Icons.favorite),
+        ],
+      ),
     );
   }
 }
