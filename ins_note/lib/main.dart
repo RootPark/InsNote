f1f2acdaf862090276e4070c447e6c99da125e07
rudolf0328 <ck0716080@gmail.com>
2022-08-24 12:41:44 +0900

    apply font

--- ins_note/lib/main.dart
+++ ins_note/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'GmarketSans',
         iconTheme: const IconThemeData(
           color: Colors.amberAccent,
         ),
