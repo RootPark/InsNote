0365352cc9aea9d6a3f090c992a1f2c867cea392
rmsdnjs518 <rmsdnjs518@gmail.com>
2022-08-25 15:44:04 +0900

    theme color change

--- ins_note/lib/main.dart
+++ ins_note/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         iconTheme: const IconThemeData(
-          color: Colors.amberAccent,
+          color: Colors.amber,
         ),
       ),
       home: const MyHomePage(),
