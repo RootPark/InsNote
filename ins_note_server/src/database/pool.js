b26b378b9eb354ee2fd00e93ddbaf075df2f30ae
2018008204 <twodf78@hanyang.ac.kr>
2022-09-30 02:57:23 +0900

    ip변경 및 service params변경

--- ins_note_server/src/database/pool.js
+++ ins_note_server/src/database/pool.js
@@ -4,8 +4,8 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
     host: "localhost",
     port: 3306,
-    user: " ",
-    password: " ", //mysql 설치시 설정했던 비번
+    user: "root",
+    password: "rmsdnjsWkd@@1212", //mysql 설치시 설정했던 비번
     database: "insnote", // db 이름
 });
 
