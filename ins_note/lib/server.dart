4ac75beab4c841e221e64eb6ff7f83ace5c0ca51
rudolf0328 <ck0716080@gmail.com>
2022-10-14 12:29:36 +0900

    server setting

--- ins_note/lib/server.dart
+++ ins_note/lib/server.dart
@@ -1,18 +1,17 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'model.dart';
 
 Future<List<Todos>> fetchTodos() async {
-  final response =
-      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+  final response = await http.get(Uri.parse('http//3.36.120.21:8080'));
 
   if (response.statusCode == 200) {
     return (jsonDecode(response.body) as List)
         .map((e) => Todos.fromJson(e))
         .toList();
   } else {
-    throw Exception('Failed to load album');
+    throw Exception('error');
   }
 }
