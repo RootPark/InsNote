a5eea87b69899a1106eacb8adfd6bc66b57caa8f
RootPark <rmsdnjs518@gmail.com>
2023-01-23 17:17:22 +0900

    bottom_sheet error delete

--- ins_note/lib/main.dart
+++ ins_note/lib/main.dart
@@ -108,40 +108,7 @@ class _MyHomePageState extends State<MyHomePage>{
           children: <Widget>[
             Padding(
               //TODO fix taglist error
-                child: FilterListWidget<User>(
-                  themeData: FilterListThemeData(context),
-                  hideSelectedTextCount: true,
-                  listData: userList,
-                  selectedListData: selectedUserList,
-                  onApplyButtonClick: (list) {
-                    Navigator.pop(context, list);
-                  },
-                  choiceChipLabel: (item) {
-                    /// Used to print text on chip
-                    return item!.name;
-                  },
-                  // choiceChipBuilder: (context, item, isSelected) {
-                  //   return Container(
-                  //     padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  //     margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  //     decoration: BoxDecoration(
-                  //         border: Border.all(
-                  //       color: isSelected! ? Colors.blue[300]! : Colors.grey[300]!,
-                  //     )),
-                  //     child: Text(item.name),
-                  //   );
-                  // },
-                  validateSelectedItem: (list, val) {
-                    ///  identify if item is selected or not
-                    return list!.contains(val);
-                  },
-                  onItemSearch: (user, query) {
-                    /// When search query change in search bar then this method will be called
-                    ///
-                    /// Check if items contains query
-                    return user.name!.toLowerCase().contains(query.toLowerCase());
-                  },
-                ),
+                
                 padding:EdgeInsets.zero
 
             ),
