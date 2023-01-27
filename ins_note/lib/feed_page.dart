08ca7dd703ba188bf906eda464118a97d7f98d08
Rudolf0328 <ck07160@hanyang.ac.kr>
2023-01-27 18:03:15 +0900

    ak

--- ins_note/lib/feed_page.dart
+++ ins_note/lib/feed_page.dart
@@ -9,8 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<List<Feed>> feedList = [] as Future<List<Feed>>;
-List<Feed> tmp = [];
+List<Feed> feedData = [];
 int listLength = 0;
 
 class FeedPage extends StatefulWidget {
@@ -23,11 +22,12 @@ class FeedPage extends StatefulWidget {
 class _FeedPageState extends State<FeedPage> {
   @override
   void initState() {
+    super.initState();
     getAllFeeds().then((value) {
-      tmp = value;
+      feedData = value;
+      print("2");
       print(value);
     });
-    super.initState();
     // print(tmp);
   }
 
@@ -46,6 +46,8 @@ class _FeedPageState extends State<FeedPage> {
           .toList();
 
       listLength = list.length;
+
+      print("1");
 
       return list;
     } else {
@@ -141,11 +143,11 @@ class _FeedPageState extends State<FeedPage> {
   Future<void> _onRefresh() {
     setState(() {
       getAllFeeds().then((value) {
-        tmp = value;
+        feedData = value;
         // feedData
       });
       print("refresh");
-      print(tmp);
+      print(feedData);
     });
     return Future<void>.value();
   }
@@ -159,98 +161,185 @@ class _FeedPageState extends State<FeedPage> {
             child: ListView.builder(
                 itemCount: listLength,
                 itemBuilder: (context, index) {
-                  // print("here?");
-                  return _getFeeds(index);
+                  String date = "";
+                  String location = "";
+                  if (feedData[index].date != null)
+                    date = feedData[index].date!;
+                  if (feedData[index].location != null)
+                    location = feedData[index].location!;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        Expanded(
+                          child: Container(
+                              margin: EdgeInsets.only(left: 15.0, right: 20.0),
+                              child: Divider(
+                                color: Colors.black,
+                                height: 36,
+                              )),
+                        ),
+                        // Visibility(
+                        //   visible: index == 0 ||
+                        //       feedData[index].date != feedData[index - 1].date,
+                        //   child: Padding(
+                        //       padding: EdgeInsets.only(right: 20),
+                        //       child: Text(date)),
+                        // )
+                      ]),
+                      Column(
+                        children: <Widget>[
+                          Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                      padding: EdgeInsets.only(bottom: 10),
+                                      child: Text(feedData[index].keyword!,
+                                          style: TextStyle(fontSize: 25))),
+                                  Padding(
+                                      padding:
+                                          EdgeInsets.only(right: 20, bottom: 5),
+                                      child: Text(feedData[index].detail!,
+                                          style: TextStyle(fontSize: 17))),
+                                  Container(
+                                      padding: EdgeInsets.only(right: 20),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                Icons.place,
+                                                color: Colors.grey,
+                                                // size: 13,
+                                              ),
+                                              Text(location,
+                                                  style: TextStyle(
+                                                      color: Colors.grey,
+                                                      fontSize: 13)),
+                                            ],
+                                          ),
+                                          Row(children: [
+                                            Visibility(
+                                                visible:
+                                                    feedData[index].link_1 !=
+                                                        "",
+                                                child: linkImageButton(
+                                                    feedData[index].link_1)),
+                                            Visibility(
+                                                visible:
+                                                    feedData[index].link_2 !=
+                                                        "",
+                                                child: linkImageButton(
+                                                    feedData[index].link_2)),
+                                            Visibility(
+                                                visible:
+                                                    feedData[index].link_3 !=
+                                                        "",
+                                                child: linkImageButton(
+                                                    feedData[index].link_3)),
+                                          ])
+                                        ],
+                                      ))
+                                ],
+                              ))
+                        ],
+                      ),
+                    ],
+                  );
                 })));
   }
 
-  Widget _getFeeds(index) {
-    // print("here2");
-    return FutureBuilder<List<Feed>>(
-        future: feedList,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final item = snapshot.data![index];
-            String location = "";
-            String link_1 = "";
-            String link_2 = "";
-            String link_3 = "";
-            if (item.location != null) location = item.location!;
-            if (item.link_1 != null) link_1 = item.link_1!;
-            if (item.link_2 != null) link_1 = item.link_2!;
-            if (item.link_3 != null) link_1 = item.link_3!;
-            // print(item.date);
+  // Widget _getFeeds(index) {
+  //   // print("here2");
+  //   return FutureBuilder<List<Feed>>(
+  //       future: feedList,
+  //       builder: (context, snapshot) {
+  //         if (snapshot.hasData) {
+  //           final item = snapshot.data![index];
+  //           String location = "";
+  //           String link_1 = "";
+  //           String link_2 = "";
+  //           String link_3 = "";
+  //           if (item.location != null) location = item.location!;
+  //           if (item.link_1 != null) link_1 = item.link_1!;
+  //           if (item.link_2 != null) link_1 = item.link_2!;
+  //           if (item.link_3 != null) link_1 = item.link_3!;
+  //           // print(item.date);
 
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Row(children: <Widget>[
-                  Expanded(
-                    child: Container(
-                        margin: EdgeInsets.only(left: 15.0, right: 20.0),
-                        child: Divider(
-                          color: Colors.black,
-                          height: 36,
-                        )),
-                  ),
-                ]),
-                Column(children: <Widget>[
-                  Container(
-                      margin: EdgeInsets.only(left: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                              padding: EdgeInsets.only(bottom: 10),
-                              child: Text(item.keyword!,
-                                  style: TextStyle(fontSize: 25))),
-                          Padding(
-                              padding: EdgeInsets.only(right: 20, bottom: 5),
-                              child: Text(item.detail!,
-                                  style: TextStyle(fontSize: 17))),
-                          Padding(
-                              padding: EdgeInsets.only(right: 20),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.place,
-                                        color: Colors.grey,
-                                      ),
-                                      Text(
-                                        location,
-                                        style: TextStyle(
-                                            color: Colors.grey, fontSize: 13),
-                                      )
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Visibility(
-                                          visible: link_1 != "",
-                                          child: linkImageButton(link_1)),
-                                      Visibility(
-                                          visible: link_2 != "",
-                                          child: linkImageButton(link_2)),
-                                      Visibility(
-                                          visible: link_3 != "",
-                                          child: linkImageButton(link_3))
-                                    ],
-                                  )
-                                ],
-                              )),
-                        ],
-                      ))
-                ])
-              ],
-            );
-          } else {
-            print("none no ever never");
-            return Text("Error");
-          }
-        });
-  }
+  //           return Column(
+  //             crossAxisAlignment: CrossAxisAlignment.stretch,
+  //             children: <Widget>[
+  //               Row(children: <Widget>[
+  //                 Expanded(
+  //                   child: Container(
+  //                       margin: EdgeInsets.only(left: 15.0, right: 20.0),
+  //                       child: Divider(
+  //                         color: Colors.black,
+  //                         height: 36,
+  //                       )),
+  //                 ),
+  //               ]),
+  //               Column(children: <Widget>[
+  //                 Container(
+  //                     margin: EdgeInsets.only(left: 20),
+  //                     child: Column(
+  //                       crossAxisAlignment: CrossAxisAlignment.start,
+  //                       children: <Widget>[
+  //                         Padding(
+  //                             padding: EdgeInsets.only(bottom: 10),
+  //                             child: Text(item.keyword!,
+  //                                 style: TextStyle(fontSize: 25))),
+  //                         Padding(
+  //                             padding: EdgeInsets.only(right: 20, bottom: 5),
+  //                             child: Text(item.detail!,
+  //                                 style: TextStyle(fontSize: 17))),
+  //                         Padding(
+  //                             padding: EdgeInsets.only(right: 20),
+  //                             child: Row(
+  //                               mainAxisAlignment:
+  //                                   MainAxisAlignment.spaceBetween,
+  //                               children: [
+  //                                 Row(
+  //                                   children: [
+  //                                     Icon(
+  //                                       Icons.place,
+  //                                       color: Colors.grey,
+  //                                     ),
+  //                                     Text(
+  //                                       location,
+  //                                       style: TextStyle(
+  //                                           color: Colors.grey, fontSize: 13),
+  //                                     )
+  //                                   ],
+  //                                 ),
+  //                                 Row(
+  //                                   children: [
+  //                                     Visibility(
+  //                                         visible: link_1 != "",
+  //                                         child: linkImageButton(link_1)),
+  //                                     Visibility(
+  //                                         visible: link_2 != "",
+  //                                         child: linkImageButton(link_2)),
+  //                                     Visibility(
+  //                                         visible: link_3 != "",
+  //                                         child: linkImageButton(link_3))
+  //                                   ],
+  //                                 )
+  //                               ],
+  //                             )),
+  //                       ],
+  //                     ))
+  //               ])
+  //             ],
+  //           );
+  //         } else {
+  //           print("none no ever never");
+  //           return Text("Error");
+  //         }
+  //       });
+  // }
 }
